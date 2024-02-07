e5ec19a29f62216815dc6ec08afe3ddd8f595dea
PropsCoder-Raj <panditrohit532@gmail.com>
2024-02-07 20:16:08 +0530

    Implemented API endpoints to retrieve completed tasks by the user and tasks due in the next 7 days. Added Swagger documentation for both endpoints.

--- src/api/controllers/tasks.js
+++ src/api/controllers/tasks.js
@@ -35,8 +35,11 @@ const { createTask, findTask, updateTask, deleteTask, taskList } = taskServices;
 */
 exports.createTask = async (req, res, next) => {
     try {
-        const task = await createTask({ ...req.body, userId: req.userId });
-        return res.status(201).send({ status: true, message: "Created Task Successfully", result: task });
+        // Create a new task with the provided data and associate it with the logged-in user
+        const taskResult = await createTask({ ...req.body, userId: req.userId });
+        
+        // Return successful response with details of the created task
+        return res.status(201).send({ status: true, message: "Created Task Successfully", result: taskResult });
     } catch (error) {
         // Return error response if an error occurs
         return res.status(500).send({ status: false, message: error.message });
@@ -70,8 +73,11 @@ exports.createTask = async (req, res, next) => {
 */
 exports.getTasksByUser = async (req, res, next) => {
     try {
-        const task = await taskList({ userId: req.userId });
-        return res.status(201).send({ status: true, message: "Get Tasks Successfully", result: task });
+        // Retrieve tasks associated with the logged-in user
+        const taskResult = await taskList({ userId: req.userId });
+        
+        // Return successful response with tasks for the user
+        return res.status(200).send({ status: true, message: "Get Tasks Successfully", result: taskResult });
     } catch (error) {
         // Return error response if an error occurs
         return res.status(500).send({ status: false, message: error.message });
@@ -115,15 +121,22 @@ exports.getTasksByUser = async (req, res, next) => {
 */
 exports.updateTask = async (req, res, next) => {
     try {
+        // Extract taskId from request parameters
         const { taskId } = req.params;
-        const isTask = await findTask({ _id: taskId, userId: req.userId })
+        
+        // Check if the task exists and belongs to the logged-in user
+        const isTask = await findTask({ _id: taskId, userId: req.userId });
 
+        // If the task is not found, return a 404 error
         if (!isTask) {
             return res.status(404).send({ status: false, message: "Task not found" });
         }
 
+        // Update the task with the provided data
         const taskResult = await updateTask({ _id: isTask._id }, { $set: req.body });
-        return res.status(201).send({ status: true, message: "Updated Task Successfully", result: taskResult });
+
+        // Return successful response with updated task details
+        return res.status(200).send({ status: true, message: "Updated Task Successfully", result: taskResult });
     } catch (error) {
         // Return error response if an error occurs
         return res.status(500).send({ status: false, message: error.message });
@@ -161,16 +174,107 @@ exports.updateTask = async (req, res, next) => {
 */
 exports.deleteTask = async (req, res, next) => {
     try {
+        // Extract taskId from request parameters
         const { taskId } = req.params;
-        const isTask = await findTask({ _id: taskId, userId: req.userId })
+        
+        // Check if the task exists and belongs to the logged-in user
+        const isTask = await findTask({ _id: taskId, userId: req.userId });
 
+        // If the task is not found, return a 404 error
         if (!isTask) {
             return res.status(404).send({ status: false, message: "Task not found" });
         }
 
+        // Delete the task
         const taskResult = await deleteTask({ _id: isTask._id });
-        console.log("task: ", taskResult)
+
+        // Return successful response with deleted task details
         return res.status(200).send({ status: true, message: "Deleted Task Successfully", result: taskResult });
+    } catch (error) {
+        // Return error response if an error occurs
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
+
+/**
+* @swagger
+* /tasks/get-completed-tasks-by-user:
+*   get:
+*     summary: Get tasks by user
+*     tags:
+*       - Task Section
+*     description: Get tasks by user
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: token
+*         description: user token
+*         in: header
+*         required: true
+*     responses:
+*       '200':
+*         description: OK
+*       '401':
+*         description: Unauthorized
+*       '402':
+*         description: Bad Request
+*       '500':
+*         description: Internal Server Error
+*/
+exports.getCompletedTasksByUser = async (req, res, next) => {
+    try {
+        // Query completed tasks for the logged-in user
+        const taskResult = await taskList({ userId: req.userId, completed: true });
+        
+        // Return successful response with completed tasks
+        return res.status(200).send({ status: true, message: "Get Completed Tasks Successfully", result: taskResult });
+    } catch (error) {
+        // Return error response if an error occurs
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
+
+
+/**
+* @swagger
+* /tasks/get-tasks-by-user-due-in-next-7-days:
+*   get:
+*     summary: Get tasks by user
+*     tags:
+*       - Task Section
+*     description: Get tasks by user
+*     produces:
+*       - application/json
+*     parameters:
+*       - name: token
+*         description: user token
+*         in: header
+*         required: true
+*     responses:
+*       '200':
+*         description: OK
+*       '401':
+*         description: Unauthorized
+*       '402':
+*         description: Bad Request
+*       '500':
+*         description: Internal Server Error
+*/
+exports.getTasksByUserDueInNext7Days = async (req, res, next) => {
+    try {
+        // Get the current date
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to 0
+
+        // Calculate the date for one week from today
+        const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+        nextWeek.setHours(23, 59, 59, 599); // Set hours, minutes, seconds, and milliseconds to the end of the day
+
+        // Query tasks due within the next 7 days for the logged-in user
+        const taskResult = await taskList({ userId: req.userId, dueDate: { $gte: today, $lte: nextWeek } });
+
+        // Return successful response with tasks due in the next 7 days
+        return res.status(200).send({ status: true, message: "Get tasks due in the next 7 days successfully", result: taskResult });
     } catch (error) {
         // Return error response if an error occurs
         return res.status(500).send({ status: false, message: error.message });
--- src/api/routes/tasks.js
+++ src/api/routes/tasks.js
@@ -1,8 +1,10 @@
 // Import the Express framework
 const express = require('express');
-const { verifyToken } = require("../../middlewares")
 
-// Import the controller module for taks operations
+// Import middleware for token verification
+const { verifyToken } = require("../../middlewares");
+
+// Import the controller module for task operations
 const controller = require('../controllers/tasks');
 
 // Create a new router instance
@@ -17,8 +19,14 @@ router.get('/get-tasks-by-user', verifyToken, controller.getTasksByUser);
 // Define a route handler for PUT requests to the '/update-tasks' endpoint
 router.put('/update-tasks/:taskId', verifyToken, controller.updateTask);
 
-// Define a route handler for PUT requests to the '/update-tasks' endpoint
+// Define a route handler for DELETE requests to the '/delete-tasks' endpoint
 router.delete('/delete-tasks/:taskId', verifyToken, controller.deleteTask);
+
+// Define a route handler for GET requests to the '/get-completed-tasks-by-user' endpoint
+router.get('/get-completed-tasks-by-user', verifyToken, controller.getCompletedTasksByUser);
+
+// Define a route handler for GET requests to the '/get-tasks-by-user-due-in-next-7-days' endpoint
+router.get('/get-tasks-by-user-due-in-next-7-days', verifyToken, controller.getTasksByUserDueInNext7Days);
 
 // Export the router instance to make it available for use in other parts of the application
 module.exports = router;
