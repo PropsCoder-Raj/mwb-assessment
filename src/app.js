bed8465ec1ab33de1587e5b904b3dda715bdff4b
PropsCoder-Raj <panditrohit532@gmail.com>
2024-02-07 11:56:18 +0530

    Swagger Doc Setup.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express'); // Express.js framework
 const morgan = require('morgan'); // HTTP request logger middleware
 const helmet = require('helmet'); // Security middleware
 const cors = require('cors'); // Cross-origin resource sharing middleware
+const swaggerJsdoc = require('swagger-jsdoc'); // Package to generate Swagger documentation
+const swaggerUi = require('swagger-ui-express'); // Package to serve Swagger UI
 
 require('dotenv').config(); // Load environment variables from .env file
 
@@ -20,6 +22,25 @@ app.use(morgan('dev')); // Log HTTP requests to the console in development mode
 app.use(helmet()); // Set various HTTP headers to secure the application
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON request bodies
+
+// Define Swagger options
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0', // OpenAPI version
+        info: {
+            title: 'MWB API Assesment Documentation', // Title of the API documentation
+            version: '1.0.0', // API version
+            description: 'This is a sample API', // Description of the API
+        },
+    },
+    apis: ['path/to/swagger.yaml'], // Path to your Swagger documentation file (update this)
+};
+
+// Generate Swagger documentation based on options
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+// Serve Swagger UI at /api-docs endpoint
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Mount API routes
 app.use('/api/v1', api); // Mount the API routes under /api/v1
